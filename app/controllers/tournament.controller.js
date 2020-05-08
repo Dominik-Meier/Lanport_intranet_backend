e7c5df8162d4ae11601d09b6d96cef568791432f
Dominik Meier <diminik.meier2@students.fhnw.ch>
2020-05-08 02:56:52 +0200

    tournament implemented

--- app/controllers/tournament.controller.js
+++ app/controllers/tournament.controller.js
@@ -1,5 +1,8 @@
 const db = require("../models");
 const Tournament = db.tournament;
+const TournamentType = db.tournamentType;
+const Lanparty = db.lanparty;
+const Gamemode = db.gamemode;
 const Op = db.Sequelize.Op;
 
 // Create and Save a new Tournament
@@ -36,7 +39,7 @@ exports.findAll = (req, res) => {
     const name = req.query.name;
     var condition = name ? { name: { [Op.like]: `%${name}%` } } : null;
 
-    Tournament.findAll({ where: condition })
+    Tournament.findAll({ where: condition, include: [TournamentType, Lanparty, Gamemode]})
         .then(data => {
             res.send(data);
         })
@@ -90,27 +93,32 @@ exports.update = (req, res) => {
 
 // Update a Tournament by the id in the request
 exports.updateAll = (req, res) => {
-    const id = req.params.id;
+    console.log(req.body);
 
-    Tournament.update(req.body, {
-        where: { id: id }
-    })
-        .then(num => {
-            if (num === 1) {
-                res.send({
-                    message: "Tournament was updated successfully."
-                });
-            } else {
-                res.send({
-                    message: `Cannot update Tournament with id=${id}. Maybe Tournament was not found or req.body is empty!`
-                });
-            }
-        })
-        .catch(err => {
-            res.status(500).send({
-                message: "Error updating Tournament with id=" + id
-            });
-        });
+    for (let tournament of req.body) {
+        if(tournament.id !== null) {
+            console.log('update tournament');
+            Tournament.update(tournament, { where: {id: tournament.id}});
+        } else if (tournament.id === null) {
+            console.log('create new tournament');
+            const newTournament = {
+                name: tournament.name,
+                description: tournament.description,
+                lanpartyId: tournament.lanparty.id,
+                gamemodeId: tournament.gameMode.id,
+                tournamentTypeId: tournament.tournamentType.id,
+                teamRegistration: tournament.teamRegistration,
+                numberOfParticipants: tournament.numberOfParticipants,
+                published: tournament.published,
+                started: tournament.started,
+                startDate: tournament.startDate,
+                finished: tournament.finished,
+            };
+            Tournament.create(newTournament);
+        }
+    }
+
+    res.status(200).send();
 };
 
 
