e2eb76b704aa2cb7dc96a0f861e0154463184be9
Dominik Meier <dominik.meier@elca.ch>
2021-08-18 00:46:53 +0200

    worked on realtime data updates

--- app/routes/angularAppConfig.routes.js
+++ app/routes/angularAppConfig.routes.js
@@ -6,5 +6,8 @@ module.exports = app => {
     router.get("/", angularAppConfig.find);
     router.post("/", angularAppConfig.create);
 
+    router.delete("/appComponent/:id", angularAppConfig.deleteAppComponent);
+    router.delete("/appRegisterComponent/:id", angularAppConfig.deleteAppRegisterComponent);
+
     app.use('/api/settings/angularAppConfig', router);
 };
