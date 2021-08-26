7881d76f81ae59744bc1f9710bf2590711d0c4e8
Dominik Meier <dominik.meier@elca.ch>
2021-08-26 16:32:30 +0200

    changed tournament creation

--- app/ControllerDelegates/tournamentDelegate.js
+++ app/ControllerDelegates/tournamentDelegate.js
@@ -1,4 +1,6 @@
 const db = require("../models");
+const {updateExistingTournament} = require("../repo/TournamentRepo");
+const {createNewTournament} = require("../repo/TournamentRepo");
 const Tournament = db.tournament;
 const TournamentType = db.tournamentType;
 const Lanparty = db.lanparty;
@@ -6,7 +8,8 @@ const Gamemode = db.gamemode;
 
 module.exports = {
     getAllTournaments: getAllTournaments,
-    updateAllTournaments: updateAllTournaments
+    updateAllTournaments: updateAllTournaments,
+    createTournament: createTournament
 }
 
 function getAllTournaments() {
@@ -16,43 +19,11 @@ function getAllTournaments() {
 async function updateAllTournaments(tournaments) {
     for (let tournament of tournaments) {
         if (tournament.id !== null) {
-            console.log('update tournament');
-            const DBTournament = {
-                id: tournament.id,
-                name: tournament.name,
-                description: tournament.description,
-                lanpartyId: tournament.lanparty.id,
-                gamemodeId: tournament.gameMode.id,
-                tournamentTypeId: tournament.tournamentType.id,
-                teamRegistration: tournament.teamRegistration,
-                numberOfParticipants: tournament.numberOfParticipants,
-                published: tournament.published,
-                started: tournament.started,
-                startDate: tournament.startDate,
-                endDate: tournament.endDate,
-                registrationEndDate: tournament.registrationEndDate,
-                finished: tournament.finished,
-                awards: tournament.awards,
-            };
-            await Tournament.update(DBTournament, { where: {id: tournament.id}});
-        } else {
-            console.log('create new tournament');
-            const newTournament = {
-                name: tournament.name,
-                description: tournament.description,
-                lanpartyId: tournament.lanparty.id,
-                gamemodeId: tournament.gameMode.id,
-                tournamentTypeId: tournament.tournamentType.id,
-                teamRegistration: tournament.teamRegistration,
-                numberOfParticipants: tournament.numberOfParticipants,
-                published: tournament.published,
-                started: tournament.started,
-                startDate: tournament.startDate,
-                endDate: tournament.endDate,
-                registrationEndDate: tournament.registrationEndDate,
-                awards: tournament.awards,
-            };
-            await Tournament.create(newTournament);
+            await updateExistingTournament(tournament);
         }
     }
+}
+
+async function createTournament() {
+    await createNewTournament();
 }
