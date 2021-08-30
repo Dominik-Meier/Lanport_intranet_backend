c8092e7169b48ced92ed3614d08c2682e70a3ec8
Dominik Meier <dominik.meier@elca.ch>
2021-08-30 17:28:54 +0200

    setup file logger

--- app.js
+++ app.js
@@ -75,13 +75,7 @@ logger = new winston.createLogger({
             maxsize: 5242880, //5MB
             maxFiles: 5,
             colorize: false
-        }),
-/*        new winston.transports.Console({
-            level: 'debug',
-            handleExceptions: true,
-            json: false,
-            colorize: true
-        })*/
+        })
     ],
     exitOnError: false
 });
