98b2b861d542a8aab590af30a48a9930c0456b56
Dominik Meier <dominik.meier@elca.ch>
2021-09-07 15:41:33 +0200

    integreated challonge

--- app/routes/challonge.routes.js
+++ app/routes/challonge.routes.js
@@ -10,7 +10,8 @@ module.exports = app => {
     router.put("/:id", auth, grantAccess('mitglied'), challonge.updateTournament);
     router.delete("/:id", auth, grantAccess('mitglied'), challonge.deleteTournament);
 
-    router.post("/participants/", auth, grantAccess('mitglied'), challonge.addParticipants)
+    router.post("/:id/participants/", auth, grantAccess('mitglied'), challonge.addParticipants)
+    router.put("/:id/participants/", auth, grantAccess('mitglied'), challonge.updateParticipants)
 
     app.use('/api/tournaments/challonge', router);
 };
