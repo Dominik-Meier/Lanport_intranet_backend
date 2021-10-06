87766c2a8aaec802ce8f54f100c46d4bedfa95e8
Dominik Meier <dominik.meier@elca.ch>
2021-10-06 18:16:26 +0200

    small changes

--- app/util/auth.js
+++ app/util/auth.js
@@ -3,9 +3,8 @@ const config = process.env;
 
 const verifyToken = (req, res, next) => {
     const token =  req.body.token || req.query.token || req.headers["authorization"];
-    if (!token) throw 'Not authorized!';
-    const splitToken = token.split(" ")
     try {
+        const splitToken = token.split(" ")
         if (splitToken.length === 2) {
             if (splitToken[0].toUpperCase() === 'BEARER') {
                 req.user = jwt.verify(splitToken[1].trim(), config.JWT_SECRET);
